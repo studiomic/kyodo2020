c7f68ca72ed6836998994e226de94bb210e9ce08
StudioMic <sakurai@studiomic.net>
2020-12-27 09:49:19 +0900

    1226
    WPSetUp
    Favicon

---
+++ config.rb
@@ -0,0 +1,22 @@
+# Set this to the root of your project when deployed:
+# cache = false
+#asset_cache_buster :none
+
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+line_comments = false
+
+# 相対パス可否
+# relative_assets = true
+
+# Disable .sass-cache
+cache = false
+
+
+# add_import_path "/Users/sakuraiizuka/Sites/JNL/Web/"
