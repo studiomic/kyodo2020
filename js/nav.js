4d66a0697a11fa7520c787b2a1fb2a1e2ef7c871
StudioMic <sakurai@studiomic.net>
2021-01-10 16:11:53 +0900

    0110
    Bus
    Media 切り分け

--- js/nav.js
+++ js/nav.js
@@ -28,6 +28,18 @@ $(function() {
 	});
 });
 
+$(function () {
+ var headerHight = 60; //ヘッダの高さ
+ $('a[href^=#]').click(function(){
+     var href= $(this).attr("href");
+       var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top-headerHight; //ヘッダの高さ分位置をずらす
+     $("html, body").animate({scrollTop:position}, 550, "swing");
+        return false;
+   });
+});
+
+
 
 // CONTACT FORM VALIDATION
 // $(function () {
