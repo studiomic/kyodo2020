fc5942c9890dc159f7eb93227a8053e52d34e6ca
StudioMic <sakurai@studiomic.net>
2021-01-30 21:37:04 +0900

    Update page-news.php

--- cocoon-child-master/page-news.php
+++ cocoon-child-master/page-news.php
@@ -7,33 +7,25 @@ Template Name: news
 <main role="main" class="news">
 	<h1>NEWS</h1>
 	<section class="container">
-		<article>
-				<time datetime="2018/01/26">
-					<span>2018</span><span>01/26</span>
-				</time>
-				<h2>お知らせ</h2>
-				<p>
-					共同タクシーは平成28年7月をもちまして終了いたしました<br>
-					フリーダイヤル（0120-944-858）は現在使用されておりません<br>
-					この番号にかけますと、他の会社に繋がってしまします。何卒ご注意くださいますようお願い申し上げます。
-				</p>
-		</article>
-		<article>
-				<time datetime="2018/3/3">
-					<span>2018</span><span>01/26</span>
-				</time>
-				<h2>お知らせ</h2>
-				<p>
-					共同タクシーは平成28年7月をもちまして終了いたしました<br>
-					フリーダイヤル（0120-944-858）は現在使用されておりません<br>
-					この番号にかけますと、他の会社に繋がってしまします。何卒ご注意くださいますようお願い申し上げます。
-				</p>
-				<p>
-					共同タクシーは平成28年7月をもちまして終了いたしました<br>
-					フリーダイヤル（0120-944-858）は現在使用されておりません<br>
-					この番号にかけますと、他の会社に繋がってしまします。何卒ご注意くださいますようお願い申し上げます。
-				</p>
-		</article>
+	<!-- SLUG-POST -->
+	<?php
+		$wp_query = new WP_Query();
+		$my_posts = array(
+			'post_type' => 'post',
+			'category_name' => 'news',
+			'posts_per_page'=> '-1',
+		);
+		$wp_query->query( $my_posts );
+		if( $wp_query->have_posts() ): while( $wp_query->have_posts() ) : $wp_query->the_post();
+	?>
+	<article id="post-<?php the_ID(); ?>">
+		<time datetime="<?php echo get_the_date(); ?>">
+			<span><?php echo get_the_date( 'Y' ); ?></span><span><?php echo get_the_date( 'm/d' ); ?></span>
+		</time>
+		<h2><?php the_title(); ?></h2>
+		<?php the_content(); ?>
+	</article>
+	<?php endwhile; endif; wp_reset_postdata(); ?>
+	<!-- SLUG-POST -->
 	</section>
-<?php get_template_part('pages/section-contactus'); ?>
 <?php get_footer(); ?>
